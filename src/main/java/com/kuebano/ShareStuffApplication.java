3663d38e470aabbced25fc0996d1306ac81cf693
Ezeokeke Kingsley <kezeokeke@seamfix.com>
2016-11-25 09:24:50 +0100

    First commit

---
+++ src/main/java/com/kuebano/ShareStuffApplication.java
@@ -0,0 +1,12 @@
+package com.kuebano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareStuffApplication.class, args);
+	}
+}
